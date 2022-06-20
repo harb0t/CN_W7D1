c9f2b48a9ed67445d0ebe9009ac80d2854ca777d
Dom Harbot <domharbot@gmail.com>
2022-06-20 19:01:24 +0100

    yarg practice

--- src/app.js
+++ src/app.js
@@ -6,25 +6,30 @@
 // const input = process.argv;
 // const movies =[];
 
-
 // -----------------------
+// -----------------------
+
 //grabs from terminal
 // console.log(input[2]);
+
 // -----------------------
+// -----------------------
+
 // const movies = [];
 // movies.push(input[2]);
 // console.log(movies);
+
+// -----------------------
 // -----------------------
 
 
+// -----------------------
 // -----------------------
 
 // if (input[2] === "add"){
 //     movies.push({title: input[3], actor: input[4]});
 //     console.log(movies);
 // }
-
-
 
 // -----------------------
 // -----------------------
@@ -66,23 +71,32 @@
 // -----------------------
 // -----------------------
 
-// With yargs
+// -----------------------
+// ------ With yargs ------
+// -----------------------
 
-// const Movie = require("./utils");
-// const yargs = require("yargs");
+const Movie = require("./utils");
 
-// console.log(yargs.argv);
-// const app = (argvArr) => {
-//   if (argvArr[2] === "add") {
-//     const movieObj = new Movie(argvArr[3], argvArr[4]);
-//     movieObj.add();
-//   } else if (argvArr[2] === "addMulti") {
-//     const movie1 = new Movie(argvArr[3], argvArr[4]);
-//     const movie2 = new Movie(argvArr[5], argvArr[6]);
-//     movie1.add();
-//     movie2.add();
-//   }
-// };
+// IMPORTING YARGS (require and package name)
+const yargs = require("yargs");
+
+console.log(yargs.argv);
+// console.log(yargs.argv.name);
+// if change line 83 to 84 with terminal command node src/App.js --name steve just steve is displayed
+
+const app = (argvArr) => {
+  if (argvArr[2] === "add") {
+    const movieObj = new Movie(argvArr[3], argvArr[4]);
+    movieObj.add();
+  } else if (argvArr[2] === "addMulti") {
+    const movie1 = new Movie(argvArr[3], argvArr[4]);
+    const movie2 = new Movie(argvArr[5], argvArr[6]);
+    movie1.add();
+    movie2.add();
+  }
+};
+
+// app(process.argv);
 
 // -----------------------
 // -----------------------
