f79e61211b547da6468ee9ca5477dd86034372d5
Dom Harbot <domharbot@gmail.com>
2022-06-20 17:08:59 +0100

    Andy's answer

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 // const Movie = require("./utils/index.js")
 // path to export
-const Movie = require("./utils")
+// const Movie = require("./utils")
 // can be shorter as node expects by default a index.js???
 
-const input = process.argv;
-const movies =[];
+// const input = process.argv;
+// const movies =[];
 
-//grabs from terminal
-// console.log(input[2]);
 
 // -----------------------
-
+//grabs from terminal
+// console.log(input[2]);
+// -----------------------
 // const movies = [];
-
 // movies.push(input[2]);
-
 // console.log(movies);
+// -----------------------
+
 
 // -----------------------
 
@@ -24,10 +24,65 @@ const movies =[];
 //     console.log(movies);
 // }
 
-if (input[2] === "add"){
-    const movieObj = new Movie(input[3], input[4]);
-    const movieObj2 = new Movie(input[5], input[6]);
-    // new key word needed when working with classes
-    movieObj.add();
-    movieObj2.add();
-}
+
+
+// -----------------------
+// -----------------------
+
+// MY ANSWER TO STRETCH
+// const Movie = require("./utils")
+
+// const input = process.argv;
+// const movies =[];
+
+// if (input[2] === "add"){
+//     const movieObj = new Movie(input[3], input[4]);
+//     const movieObj2 = new Movie(input[5], input[6]);
+//     // new key word needed when working with classes
+//     movieObj.add();
+//     movieObj2.add();
+// }
+
+// -----------------------
+// -----------------------
+
+// ANDY'S STRECTH ANSWER
+// const Movie = require("./utils");
+
+// const app = (argvArr) => {
+// if (argvArr[2] === "add") {
+//     const movieObj = new Movie(argvArr[3], argvArr[4]);
+//     movieObj.add();
+//     } else if (argvArr[2] === "addMulti") {
+//     const movie1 = new Movie(argvArr[3], argvArr[4]);
+//     const movie2 = new Movie(argvArr[5], argvArr[6]);
+//     movie1.add();
+//     movie2.add();
+//     }
+// };
+
+// app(process.argv);
+
+// -----------------------
+// -----------------------
+
+// With yargs
+
+// const Movie = require("./utils");
+// const yargs = require("yargs");
+
+// console.log(yargs.argv);
+// const app = (argvArr) => {
+//   if (argvArr[2] === "add") {
+//     const movieObj = new Movie(argvArr[3], argvArr[4]);
+//     movieObj.add();
+//   } else if (argvArr[2] === "addMulti") {
+//     const movie1 = new Movie(argvArr[3], argvArr[4]);
+//     const movie2 = new Movie(argvArr[5], argvArr[6]);
+//     movie1.add();
+//     movie2.add();
+//   }
+// };
+
+// -----------------------
+// -----------------------
